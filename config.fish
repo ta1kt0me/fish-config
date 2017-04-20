10e2c0ac9b6237e2bb26199ef8866792836b3a33
ta1kt0me <p.wadachi@gmail.com>
2017-04-21 01:46:22 +0900

    Load hook script
    refs: https://github.com/5t111111/fish-config/blob/ebce2795574c5a2ee2f71afb3f7a7aedf2cf784f/fish/config.fish#L4-L21

--- config.fish
+++ config.fish
@@ -1,2 +1,21 @@
 set -x MANPATH "/usr/local/share/fish/man:"
 set -g Z_SCRIPT_PATH /usr/local/opt/z/etc/profile.d/z.sh
+
+begin
+    set -l source_dirs hooks
+    set -l fish_config_dir "$HOME/.config/fish"
+
+    pushd $fish_config_dir
+
+    for dir in $source_dirs
+        if test -d $dir; and not test (git check-ignore $dir)
+            for src in (ls $dir)
+                if not test (git check-ignore $dir/$src)
+                    source $dir/$src
+                end
+            end
+        end
+    end
+
+    popd
+end
