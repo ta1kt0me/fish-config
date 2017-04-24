4a70955a289da70facb112f6628bf4658a156fd9
ta1kt0me <p.wadachi@gmail.com>
2017-04-25 00:14:12 +0900

    Abbr for git

--- abbrs/git.fish
+++ abbrs/git.fish
@@ -1,3 +1,7 @@
 abbr -a gsb git status -sb
 abbr -a gcb git checkout -b
 abbr -a gcop peco_checkout_branch
+abbr -a gco git checkout
+abbr -a gcobr peco_checkout_remote_branch
+abbr -a ggr git grep
+abbr -a ggsed replace_git_grep
