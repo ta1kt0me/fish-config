5b96c1704dbca91ae1d5e0668b0687bf89c8249f
ta1kt0me <p.wadachi@gmail.com>
2017-05-23 01:45:46 +0900

    Setup go and node environment

---
+++ profiles/go.fish
@@ -0,0 +1,3 @@
+set -x GOPATH $HOME/Dev/GOPATH
+set -x PATH $HOME/.goenv/bin $GOPATH/bin $PATH
+eval (goenv init -)
---
+++ profiles/node.fish
@@ -0,0 +1,1 @@
+set -x PATH $HOME/.nodebrew/current/bin ./node_modules/.bin $PATH
