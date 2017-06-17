0c3f187b82cc95522323bf976aee72145838a65e
ta1kt0me <p.wadachi@gmail.com>
2017-06-17 16:36:24 +0900

    Start up ssh-agent in start up fish

---
+++ profiles/ssh_agent.fish
@@ -0,0 +1,18 @@
+function start_ssh_agent -d 'start ssh-agent'
+    setenv SSH_ENV $HOME/.ssh/environment
+
+    if not test -e $SSH_ENV
+        ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+        chmod 600 $SSH_ENV
+    end
+
+    . $SSH_ENV > /dev/null ^ /dev/null
+    ssh-add > /dev/null ^ /dev/null
+
+    if not test $status -eq '0'
+        rm $SSH_ENV
+        start_ssh_agent
+    end
+end
+
+start_ssh_agent
