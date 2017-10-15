911e822d3ea2fe629896d0cc7598540c11b1663d
ta1kt0me <p.wadachi@gmail.com>
2017-10-15 22:07:25 +0900

    Show notifier only if install terminal-notifier

--- hooks/notify_postexec.fish
+++ hooks/notify_postexec.fish
@@ -1,15 +1,17 @@
 function notify_postexec --on-event fish_postexec
-    set -l cmd_res $status
-    string match -r '^bundle$|bundle install| be |bundle exec|bin' $argv | read match
-    string length -q $match
-    if [ $status = 0 ]
-        if [ $cmd_res = 0 ]
-            set -x res "🍏🍏🍏"
-        else
-            set -x res "🍎🍎🍎"
-        end
+    if test -n "(command -s terminal-notifier)"
+        set -l cmd_res $status
+        string match -r '^bundle$|bundle install| be |bundle exec|bin' $argv | read match
+        string length -q $match
+        if [ $status = 0 ]
+            if [ $cmd_res = 0 ]
+                set -x res "🍏🍏🍏"
+            else
+                set -x res "🍎🍎🍎"
+            end
 
-        terminal-notifier -title "😎$argv" -message "$res Complete" -sound Submarine -timeout 10 -activate "com.googlecode.iterm2" > /dev/null &
-        set -e res
+            terminal-notifier -title "😎$argv" -message "$res Complete" -sound Submarine -timeout 10 -activate "com.googlecode.iterm2" > /dev/null &
+            set -e res
+        end
     end
 end
