2d7c09d390c6916840a8a43fbdeb25f0758c9b71
ta1kt0me <p.wadachi@gmail.com>
2018-08-26 23:30:09 +0900

    Fix to setup go env

--- profiles/go.fish
+++ profiles/go.fish
@@ -1,4 +1,6 @@
-set -x GOPATH $HOME/Dev/GOPATH
-set -x PATH $GOPATH/bin $PATH
-set -x PATH $HOME/.goenv/bin $PATH
+set -x GOPATH $HOME/src
+set -x GOBIN $GOPATH/bin
+set -x PATH $GOBIN $PATH
+set -x GOENV_ROOT $HOME/.goenv
+set -x PATH $GOENV_ROOT/bin $PATH
 goenv rehash >/dev/null ^&1
