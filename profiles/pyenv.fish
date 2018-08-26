df24ed3c153aad5d1b1971db43eefff9e4149416
ta1kt0me <p.wadachi@gmail.com>
2018-08-27 00:09:35 +0900

    Install xxenv by cloning from github

--- profiles/pyenv.fish
+++ profiles/pyenv.fish
@@ -1,1 +1,3 @@
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
 string replace -a 'setenv' 'set -x' (pyenv init -) | source
--- profiles/rbenv.fish
+++ profiles/rbenv.fish
@@ -1,1 +1,3 @@
+set -x RBENV_ROOT $HOME/.rbenv
+set -x PATH $RBENV_ROOT/bin $PATH
 string replace -a 'setenv' 'set -x' (rbenv init -) | source
