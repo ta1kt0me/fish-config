4df24712e0541d9b2256e6a62024329fec2f0d46
ta1kt0me <p.wadachi@gmail.com>
2018-10-11 20:25:31 +0900

    Enable go command

--- profiles/go.fish
+++ profiles/go.fish
@@ -4,6 +4,7 @@ set -x PATH $GOBIN $PATH
 set -x GOENV_ROOT $HOME/.goenv
 set -x PATH $GOENV_ROOT/bin $PATH
 goenv rehash >/dev/null ^&1
+string replace -a 'setenv' 'set -x' (goenv init -) | source
 
 # direnv
 eval (direnv hook fish)
