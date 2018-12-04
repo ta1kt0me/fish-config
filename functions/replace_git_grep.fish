8b9af437b0558936f0aa02622a40a9b0c30e6572
ta1kt0me <p.wadachi@gmail.com>
2018-12-04 11:02:29 +0900

    Avoid no file or directory exists error

--- functions/replace_git_grep.fish
+++ functions/replace_git_grep.fish
@@ -1,5 +1,5 @@
 function replace_git_grep -d "Replace string by git grep"
     set target $argv[1]
     set replace $argv[2]
-    git grep -l $target | xargs sed -i '' -e "s/$target/$replace/g"
+    git grep -l $target | xargs sed --in-place='' -e "s/$target/$replace/g"
 end
