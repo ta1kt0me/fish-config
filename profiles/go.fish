afa45e50639792ea49bb621e519771a6f9b101a2
ta1kt0me <p.wadachi@gmail.com>
2019-05-29 01:35:44 +0900

    Fix invalid path value

--- profiles/go.fish
+++ profiles/go.fish
@@ -1,9 +1,8 @@
+set -x GOENV_DISABLE_GOPATH 1
 set -x GOPATH $HOME/src
-set -x GOBIN $GOPATH/bin
-set -x PATH $GOBIN $PATH
 set -x GOENV_ROOT $HOME/.goenv
 set -x PATH $GOENV_ROOT/bin $PATH
-goenv rehash >/dev/null ^&1
+set -x PATH $GOPATH/bin $PATH
 string replace -a 'setenv' 'set -x' (goenv init -) | source
 
 # direnv
