2a0e0a89eadc59c1dd91cedfefd917a5690f9d84
ta1kt0me <p.wadachi@gmail.com>
2019-06-02 18:54:48 +0900

    Refactor to set go path

--- profiles/go.fish
+++ profiles/go.fish
@@ -1,9 +1,11 @@
-set -x GOENV_DISABLE_GOPATH 1
-set -x GOPATH $HOME/src
 set -x GOENV_ROOT $HOME/.goenv
 set -x PATH $GOENV_ROOT/bin $PATH
-set -x PATH $GOPATH/bin $PATH
 string replace -a 'setenv' 'set -x' (goenv init -) | source
+set -x PATH $GOPATH/bin $PATH
 
 # direnv
-eval (direnv hook fish)
+type -f direnv > /dev/null
+
+if test $status -eq 0
+    eval (direnv hook fish)
+end
