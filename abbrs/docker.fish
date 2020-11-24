ba48d878383f467e51457f21742702cc5f2952e4
ta1kt0me <p.wadachi@gmail.com>
2020-11-25 01:40:05 +0900

    Add abbr for awscli in docker

---
+++ abbrs/docker.fish
@@ -0,0 +1,3 @@
+abbr -a gsb git status -sb
+abbr -a daws docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli
+abbr -a deaws docker run --env-file .env --rm -it -v ~/.aws:/root/.aws amazon/aws-cli
