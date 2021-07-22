8d5f0730138a06c4aa1c408206ac26d84e402868
ta1kt0me <p.wadachi@gmail.com>
2021-07-22 22:52:40 +0900

    stop ssh-agent before restart

--- profiles/ssh_agent.fish
+++ profiles/ssh_agent.fish
@@ -2,6 +2,8 @@ function start_ssh_agent -d 'start ssh-agent'
     setenv SSH_ENV /tmp/environment
 
     if not test -e $SSH_ENV
+        echo "[start_ssh_agent] start up ssh-agent"
+        killall ssh-agent
         ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
         chmod 600 $SSH_ENV
     end
@@ -11,6 +13,7 @@ function start_ssh_agent -d 'start ssh-agent'
 
     if not test $status -eq '0'
         rm $SSH_ENV
+        echo "[start_ssh_agent] restart ssh-agent"
         start_ssh_agent
     end
 end
