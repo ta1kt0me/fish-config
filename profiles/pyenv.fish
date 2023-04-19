f2c8ef8ae00b6010547887c2862ca59cc459c1a5
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2023-04-19 17:28:33 +0900

    update pyenv setting

--- profiles/pyenv.fish
+++ profiles/pyenv.fish
@@ -1,3 +1,4 @@
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
