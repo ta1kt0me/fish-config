d5528a089420b59cb9f0bffcf71966abc92e2821
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2024-03-16 02:00:55 +0900

    don't load as universal

--- profiles/pyenv.fish
+++ profiles/pyenv.fish
@@ -1,5 +1,5 @@
 if test -z "$PYENV_ROOT"
-  set -Ux PYENV_ROOT $HOME/.pyenv
+  set -gx PYENV_ROOT $HOME/.pyenv
   fish_add_path $PYENV_ROOT/bin
   status is-login; and pyenv init --path | source
   status is-interactive; and pyenv init - | source
